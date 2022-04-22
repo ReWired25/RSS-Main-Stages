7cd4f00a71ff449763d52f5d007af802834f5c28
Nikita K <w.feelingalright.p@yandex.ru>
2022-04-22 20:57:31 +0300

    feat: add burger menu functional

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,3 +1,59 @@
+// *************** selectors, variables *************** //
+
+const burgerMenu = document.querySelector('.burger-menu');
+const mainMenu = document.querySelector('.main-menu');
+const menuLogo = document.querySelector('.menu-logo');
+const overlay = document.querySelector('.overlay');
+
+function toggleMenu() {
+    let arr = [burgerMenu, mainMenu, menuLogo, overlay, document.body];
+    for (let elem of arr) {
+        elem.classList.toggle('open');
+    }
+}
+
+burgerMenu.addEventListener('click', () => {
+        toggleMenu();
+})
+
+mainMenu.addEventListener('click', (event) => {
+    if (event.target.classList.contains('menu__link')) {
+        toggleMenu();
+    }
+})
+
+overlay.addEventListener('click', () => {
+    toggleMenu();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(`Выполненная самопроверка второй части задания "Shelter":
 Общая проверка поведения приложения при изменении ширины окна:
 
