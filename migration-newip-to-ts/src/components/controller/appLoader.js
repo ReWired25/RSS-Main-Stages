ddb2c2fc40059115cf30cb905a1776abe686e900
Nikita K <w.feelingalright.p@yandex.ru>
2022-07-01 03:29:28 +0300

    feat: add new api key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '7d5f5b3ecb15471885fc6585166838aa', // получите свой ключ https://newsapi.org/
         });
     }
 }
