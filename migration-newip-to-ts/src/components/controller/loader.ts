6b62dd8a5278973e5427113835623c3f633cab71
Nikita K <w.feelingalright.p@yandex.ru>
2022-07-02 04:12:35 +0300

    feat: migration loader.js to .ts from controller component complete

---
+++ migration-newip-to-ts/src/components/controller/loader.ts
@@ -0,0 +1,58 @@
+import { IDataSource, IDataNews } from '../../types/interfaces';
+
+type getRestObjParameter = {
+    endpoint: string;
+    options?: Record<string, string>;
+};
+
+type Callback<T> = (data: T) => void;
+
+class Loader {
+    private baseLink: string;
+    private options: { apiKey: string };
+
+    constructor(baseLink: string, options: { apiKey: string }) {
+        this.baseLink = baseLink;
+        this.options = options;
+    }
+
+    getResp(
+        { endpoint, options = {} }: getRestObjParameter,
+        callback = () => {
+            console.error('No callback for GET response');
+        }
+    ): void {
+        this.load('GET', endpoint, callback, options);
+    }
+
+    errorHandler(res: Response) {
+        if (!res.ok) {
+            if (res.status === 401 || res.status === 404)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+
+        return res;
+    }
+
+    makeUrl(options: Record<string, string>, endpoint: string) {
+        const urlOptions: Record<string, string> = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    load(method: string, endpoint: string, callback: Callback<IDataSource | IDataNews>, options = {}) {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data: IDataSource | IDataNews) => callback(data))
+            .catch((err: Error) => console.error(err));
+    }
+}
+
+export default Loader;
--- migration-newip-to-ts/src/types/interfaces.ts
+++ migration-newip-to-ts/src/types/interfaces.ts
@@ -1,15 +1,3 @@
-export interface IResponse {
-    type: string;
-    bodyUsed: boolean;
-    body: object;
-    headers: object;
-    ok: boolean;
-    redirected: boolean;
-    status: number;
-    statusText: string;
-    url: string;
-}
-
 type SourceInfo = {
     id: string;
     name: string;
