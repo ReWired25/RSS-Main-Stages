870ab7ad35d2188c7cece97026abe1c741669a38
Nikita K <w.feelingalright.p@yandex.ru>
2022-07-04 23:03:16 +0300

    fix: change api link to universal link for deploy

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '7d5f5b3ecb15471885fc6585166838aa', // получите свой ключ https://newsapi.org/
         });
     }
